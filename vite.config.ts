6a761d3dd52fe12758878ed017bcb83a920486ba
juanruizumbrete-droid <juanruizumbrete@gmail.com>
2026-02-07 13:22:37 +0100

    Update Vite base configuration for deployment
    Change base configuration for Vite to use relative paths.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Esto asegura que Vercel busque los archivos en la carpeta correcta tras construir la web
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  // Al dejar la base vacía, Vite usará rutas relativas que Vercel entiende mejor
+  base: './', 
 })
